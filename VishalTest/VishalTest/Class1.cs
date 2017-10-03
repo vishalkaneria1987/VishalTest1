3f61fd1883ca84d22bdefcb35cb3b9fa2b103903
vishalkaneria1987 <vishalkaneria1987@gmail.com>
2017-10-03 16:01:45 +0530

    VIshal test

--- VishalTest/VishalTest/Class1.cs
+++ VishalTest/VishalTest/Class1.cs
@@ -4,27 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace VishalTest
 {
-    [TestClass]
+
     public class Program
     {
-        [TestMethod]
-      public static void Main(String[] arg)
+        private static void Main(String[] arg)
         {
             Console.WriteLine(CreateMessage());
         }
         public static string CreateMessage()
+
         {
             return "Vishal";
         }
-    }
-
-    internal class TestMethodAttribute : Attribute
-    {
-    }
-
-    internal class TestClassAttribute : Attribute
-    {
     }
 }
