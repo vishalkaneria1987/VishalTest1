8bc13ef7ce4b400dd577a771123d9abadf3b79df
vishalkaneria1987 <vishalkaneria1987@gmail.com>
2017-10-10 23:50:55 +0530

    Logout

--- src/jdbcpackage/Logout.java
+++ src/jdbcpackage/Logout.java
@@ -1,20 +1,17 @@
 package jdbcpackage;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Logout {
-	
-	
+		
 	public void logout(WebDriver driver)
 	{
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.findElement(By.id("logout")).click();
-		
-		
+				
 	}
 
-	
-}
+	}
