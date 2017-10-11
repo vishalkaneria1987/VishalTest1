f177005d78ca0586c23a205cbaa17bd6d820d0ef
vishalkaneria1987 <vishalkaneria1987@gmail.com>
2017-10-11 11:01:05 +0530

    Modify Import

--- src/jdbcpackage/Logout.java
+++ src/jdbcpackage/Logout.java
@@ -3,6 +3,7 @@ package jdbcpackage;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Logout {
